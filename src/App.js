0ff05c1881fa07e4836d89b92d7be92cafa92878
robinhood8865 <robinhood8865@gmail.com>
2022-06-04 09:46:49 +0900

    nav bar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Navbar" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
