212337bef99fc1412be8a6b90d4073f9ce36863b
Ace Hood <101823587+acehood0126@users.noreply.github.com>
2022-06-05 14:41:30 +0900

    whatsinside

--- src/components/layouts/Whatsinside.js
+++ src/components/layouts/Whatsinside.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Whatsinside = () => {
   return (
-    <div className="w-full px-[60px] py-[45px]">
+    <div className="w-full px-[60px] py-[45px] grid grid-cols-1 justify-items-center">
       <div></div>
+      <Link to="/">
+        <button className="transition bg-lightgreen hover:bg-darkgreen ease-out hover:ease-in rounded-[6px] text-white text-[14px] font-bold px-[12px] py-[6px] w-[170px] h-[40px]">
+          Get Started
+        </button>
+      </Link>
     </div>
   );
 };
