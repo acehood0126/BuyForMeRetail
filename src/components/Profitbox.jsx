5f7f72787c5bfd1f74729c39a62fedac6edcc6cb
Ace Hood <101823587+acehood0126@users.noreply.github.com>
2022-06-07 22:34:40 +0900

    profit box

--- src/components/Profitbox.jsx
+++ src/components/Profitbox.jsx
@@ -1,6 +1,6 @@
 const Profitbox = ({ pic, heading, txt }) => {
   return (
-    <div className="bg-specwhite drop-shadow-threeDbox hover:drop-shadow-none transition ease-linear min-w-[350px] mb-[20px] px-[20px] py-[20px] rounded-[6px] grid justify-items-center content-start">
+    <div className="bg-specwhite drop-shadow-threeDbox hover:drop-shadow-none transition ease-linear max-w-[350px] mb-[20px] px-[20px] py-[20px] rounded-[6px] grid justify-items-center content-start">
       <img src={pic} className="mb-[18px]" alt="" />
       <div>
         <h3 className="font-bold text-[16px] mb-[5px] text-center">
