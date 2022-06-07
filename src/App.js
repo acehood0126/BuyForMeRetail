5d6e80ddf9cdd366cbec674c90ea1226e01f1964
robinhood8865 <robinhood8865@gmail.com>
2022-06-08 08:50:18 +0900

    whatsnew

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Forgot from "./pages/Forgot";
+import WhatsNew from "./pages/WhatsNew";
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +16,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot" element={<Forgot />} />
+        <Route path="/whatsnew" element={<WhatsNew />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/WhatsNew.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const WhatsNew = () => {
+  return (
+    <div className="">
+      <div></div>
+    </div>
+  );
+};
+
+export default WhatsNew;
