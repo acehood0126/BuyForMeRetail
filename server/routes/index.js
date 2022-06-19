4e568acce38bacc5fe9ee7f473efb95de4e59623
robinhood8865 <robinhood8865@gmail.com>
2022-06-19 10:05:47 +0900

    node start

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
